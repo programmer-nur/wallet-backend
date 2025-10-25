9407ecc9029ef35c4bd83565164699b849969758
programmer-nur <pesfootball83@gmail.com>
2025-10-25 12:19:42 +0600

    add

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
@@ -13,6 +14,12 @@ const app = express();
 if (process.env.NODE_ENV === "production") job.start();
 
 // middleware
+app.use(
+  cors({
+    origin: "*", // Allow all origins in development
+    credentials: true,
+  })
+);
 app.use(rateLimiter);
 app.use(express.json());
 
